849e065124a0384caf01599098b4b358d9cb9fae
Wilsom <wilsomli@qq.com>
2017-08-03 14:19:16 +0800

    Update math_ext.lua
    math ext functions

--- Assets/Lua/Common/math_ext.lua
+++ Assets/Lua/Common/math_ext.lua
@@ -1,4 +1,22 @@
 
+--数学相关辅助函数
+
+local math = math
+
 function math.round(num)
 	return math.floor(num + 0.5)
+end
+
+function math.int(x)
+	return math.modf(x)
+end
+
+function math.clamp(value,min,max)
+	if value < min then
+		return min
+	elseif value > max then
+		return max
+	else
+		return value
+	end
 end
