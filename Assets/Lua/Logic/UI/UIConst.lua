91e9e72f3e75276ed7d29a6f9853cb54c2965e30
Wilsom <wilsomli@qq.com>
2017-08-09 10:16:42 +0800

    update

--- Assets/Lua/Logic/UI/UIConst.lua
+++ Assets/Lua/Logic/UI/UIConst.lua
@@ -2,11 +2,14 @@
 UIConst = {}
 
 UIConst.Layer = {
-	
+	UI = "UI",
+	Top = "Top",
+	Broadcast = "Broadcast",
 }
 
 UIConst.Login = {
-	name = "Login"
+	name = "Login",
+	layer = UIConst.Layer.UI,
 }
 
 
