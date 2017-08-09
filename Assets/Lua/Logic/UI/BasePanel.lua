4e2bcb38ec670b4c16d95fd7cef30879bbf15af5
Wilsom <wilsomli@qq.com>
2017-08-09 09:52:35 +0800

    update

--- Assets/Lua/Logic/UI/BasePanel.lua
+++ Assets/Lua/Logic/UI/BasePanel.lua
@@ -5,7 +5,12 @@ function BasePanel:MapNode(cfg)
 	for k,v in pairs(cfg) do
 		local tf = self.transform:Find(v.path)
 		if tf then
-			-- tf.gameObject
+			local obj = v.type and tf:GetComponent(typeof(v.type)) or tf.gameObject
+			if obj then
+				if v.onClick then
+					
+				end
+			end			
 		end
 	end
 end
